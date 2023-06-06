4d97a0bceef01b571c1d6186eed441a9c09576dc
oearlgrey <o_earlgre@naver.comy>
2023-06-07 00:11:33 +0900

    hh

---
+++ pearl0605/pearlabyss-ex/js/common.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+
+    let newsSlider = new Swiper(".news_station",{
+        pagination: {
+            el: '.news_pagination',
+            clickable: true,
+        },
+        breakpoints:{
+            280:{
+                slidesPerView:1,
+            },
+            768:{
+                slidesPerView:3,
+            },
+        }
+    })
+    let noticeSlider= new Swiper(".notice_station",{
+        pagination: {
+            el: '.notice_pagination',
+            clickable: true,
+        },
+        breakpoints:{
+            280:{
+                slidesPerView:1,
+            },
+            768:{
+                slidesPerView:4,
+                spaceBetween: 20,
+            },
+        }
+
+    })
+        
+}
+
