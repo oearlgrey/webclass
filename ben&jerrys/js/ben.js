db01df397828f2b26dbbc4def947d0754dcbcb51
oearlgrey <o_earlgrey@naver.com>
2023-06-08 18:12:37 +0900

    h

--- ben&jerrys/js/ben.js
+++ ben&jerrys/js/ben.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $(".nationSelect").click(function(){
-        if($(".nationSelect").hasClass("on")==false){
+        if($(".nationList").hasClass("on")==false){
             //메뉴가 닫혀있는 상태 -> 여는 기능
-            $(".nationSelect").addClass("on")
+            $(".nationList").addClass("on")
 
         }else{
             //메뉴가 열려있는 상태 -> 메뉴를 닫는 기능
-            $(".nationSelect").removeClass("on")
+            $(".nationList").removeClass("on")
          
         }
     });
