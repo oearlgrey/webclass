14c41baf7ddb246009dbdec36c0387fbe2764db6
oearlgrey <o_earlgrey@naver.com>
2023-06-20 17:22:46 +0900

    jyzzang

--- 시험/garo/js/main.js
+++ 시험/garo/js/main.js
@@ -1,26 +1,54 @@
-$(document).ready(function(){
-    //가로형
-    let count = 0;
+// $(document).ready(function(){
+//     //가로형
+//     let count = 0;
 
+//     setInterval(function(){
+//         count++;
+//         if(count>2){count=0}
+//         $(".train").css("transform","translateX("+(-1200*count)+"px)")
+//                                 //("transform","translateX("+(-33.333*count)+"%)")  
+//     },2500)
+
+//     //세로형
+//     // setInterval(function(){
+//     //     count++;
+//     //     if(count>2){count=0}
+//     //     $(".train").css("transform","translateY("+(-33.333*count)+"%)")  
+//     // },2500)
+
+
+//     $(".tabmenu>dt").click(function(){
+//         $(".tabmenu>dt").removeClass("on")
+//         $(this).addClass("on")
+    
+//         $(".tabmenu>dd").removeClass("on")
+//         $(this).next().addClass("on")
+//     })
+
+//     $(".btnpopup").click(function(){
+//         $(".popup").addClass("on")
+//     })
+
+//     $(".popclose").click(function(){
+//         $(".popup").removeClass("on")
+//     })
+// })
+
+$(document).ready(function(){
+
+    let count = 0 ;
+    
     setInterval(function(){
         count++;
         if(count>2){count=0}
-        $(".train").css("transform","translateX("+(-1200*count)+"px)")
-                                //("transform","translateX("+(-33.333*count)+"%)")  
+         $(".train").css("transform","translateX("+(-1200*count)+"px)")
     },2500)
-
-    //세로형
-    // setInterval(function(){
-    //     count++;
-    //     if(count>2){count=0}
-    //     $(".train").css("transform","translateY("+(-33.333*count)+"%)")  
-    // },2500)
-
+    
 
     $(".tabmenu>dt").click(function(){
         $(".tabmenu>dt").removeClass("on")
         $(this).addClass("on")
-    
+        
         $(".tabmenu>dd").removeClass("on")
         $(this).next().addClass("on")
     })
@@ -33,4 +61,3 @@ $(document).ready(function(){
         $(".popup").removeClass("on")
     })
 })
-
