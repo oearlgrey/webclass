7035adff621a7ffc9eacb250d24635572781cd66
oearlgrey <o_earlgrey@naver.com>
2023-06-30 17:52:13 +0900

    hq

--- 41.svg/js/study6.js
+++ 41.svg/js/study6.js
@@ -1,35 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     let pathLength = $("#progress path").get(0).getTotalLength()
     $("#progress path").css("stroke-dasharray",pathLength)
     $("#progress path").css("stroke-dashoffset",pathLength)
 
-    $(window).scroll(function(){
-        let winst = $(window).scrollTop()
-        let fixedTop = $(".fixedFrame").offset().top
-        let distance = winst-fixedTop
-        let movingAreaheight = $(".movingArea").height()
-    
 
-        if(distance<0){
-            $(".movingArea").css("position","absolute")
-            $(".movingArea").css("left","0")
-            $(".movingArea").css("top","0")
-        }
+    $(window).scroll(function () {
+      let winTop = $(window).scrollTop()
+      let fixedTop = $(".fixedFrame").offset().top
+      let distance = winTop - fixedTop
+      let movingAreaHeight = $(".movingArea").height()
+       
+      if (distance < 0) {
+        $(".movingArea").css("position", "absolute")
+        $(".movingArea").css("top", "0")
+        $(".movingArea").css("left", "0")
+      }
+  
+      if (distance >= 0 && distance <= 8000 - movingAreaHeight) {
+        $(".movingArea").css("position", "fixed")
+        $(".movingArea").css("top", "0")
+        $(".movingArea").css("left", "0")
+        // console.log(distance)
 
-        if(distance >=0 && distance <= 8000 - movingAreaheight){
-            $(".movingArea").css("position","fixed")
-            $(".movingArea").css("left","0")
-            $(".movingArea").css("top","0")
-            let percent = (distance/(8000 - movingAreaheight)*100)
-            $("#progress path").css("stroke-dashoffset",pathLength-(pathLength*percent/100))
-            $(".per").text(Math.round(percent)+"%")
-        }
-        
-        if(distance > 8000 - movingAreaheight){
-            $(".movingArea").css("position","absolute")
-            $(".movingArea").css("top",(8000 - movingAreaheight)+"px")
-        }
+        let percent = (distance / (8000 - movingAreaHeight) * 100) // 0~100 까지의 수
+        $("#progress path").css("stroke-dashoffset",pathLength-(pathLength*percent/100))
+        $(".per").text(Math.round(percent)+"%")
+
+        let percent2 = (distance / (8000 - movingAreaHeight) * 87.5) // 0~87.5 까지의 수
+        $(".train").css("transform","translateX(-"+percent2+"%)")  
+      }
+  
+      if (distance > 8000 - movingAreaHeight) {
+        $(".movingArea").css("position", "absolute")
+        $(".movingArea").css("top", (8000 - movingAreaHeight) + "px")
+        $(".movingArea").css("left", "0")
+      }
     })
-})
-//winsTop-fixedTop >=0 && winsTop-fixedTop < 7200 ex 7200보다 작을 때까지 스크롤이벤트 적용
+  
+  })
+
+   // let count = Math.floor(distance/(8000-movingAreaHeight)*8) //0~7사이의 정수
+        // let count2 = distance/(8000-movingAreaHeight)*8 // 0~1 사이의 소수
+        // console.log(count+"번째 리스트의 애니메이션이 "+count2+"만큼 진행되었습니다.")
+        // $(".train>li").removeClass("on")
+        // $(".train>li").eq(count).addClass("on")
+
+        // let count3 = count2*180
+        // $(".train li b").css("transform","rotateY("+(180+count3)+"deg)")
